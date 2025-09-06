ae541db41e0489b32e03ea18d139e6f150b52e88
aboy4321 <awboyce630@gmail.com>
2025-09-06 15:42:55 -0400

    added set last input function and start of Bounds class

--- proto.cpp
+++ proto.cpp
@@ -1,18 +1,39 @@
 #include <iostream>
 #include <vector>
 #include <memory>
+#include <algorithm>
+#include <utility>
+#include <stdexcept>
 
 using namespace std; 
 
 class Bounds{
     private:
-        int lb, ub;
+        int lower_bound, upper_bound;
         
-        Bounds();
+        Bounds(int lb, int ub) : lower_bound(lb), upper_bound(ub) {}
 
-        static pair<int, int> from_weights 
+        static Bounds from_weights(const vector<int>& weights, int size) {
+            int lb, ub = 0, 0;
+            
+            int end_pt = min(size, static_cast<int>(weights.size()))
 
+            for (int i = 0; i < range; i++) {
+                int weight = weights[i];
 
+                if (weight < 0) {
+                    lb += weight;
+                }
+                if (weight > 0) {
+                    ub += weight;
+                }
+            }
+            return Bounds(lb, ub);
+        } 
+
+    pair<int, int> get_bounds() const {
+        return make_pair(lower_bound, upper_bound);
+    }
 }
 
 class ThresholdTest{
@@ -39,13 +60,25 @@ class ThresholdTest{
 
         shared_ptr<ThresholdTest> set_last(int value) const {
             int last_weight = get_last();
-            
-            if(value == 1) {
+            double nu_threshold = threshold;
+
+            if (value == 1) {
                 int nu_threshold = threshold - last_weight;
             }
-            if(value == 0) {
+            if (value == 0) {
                 int nu_threshold = threshold;
             }
+
+            auto [lb, ub] = bounds.get_bounds();
+
+            if (last_weight > 0) {
+                ub -= last_weight;
+            } else {
+                lb -= last_weight;
+            }
+            Bounds nu_bounds(lb, ub);
+
+            return make_shared<ThresholdTest>(weights, nu_threshold, indices, size - 1, nu_bounds);
 
         }
 }
