dd77d388e87416df0f663799bd6fd173c0e6c1de
aboy4321 <awboyce630@gmail.com>
2025-09-13 23:27:43 -0400

    added sort_weights function

--- proto.cpp
+++ proto.cpp
@@ -10,17 +10,16 @@ using namespace std;
 class Bounds{
     private:
         int lower_bound, upper_bound;
-        
+    
+    public:     
         Bounds(int lb, int ub) : lower_bound(lb), upper_bound(ub) {}
 
         static Bounds from_weights(const vector<int>& weights, int size) {
             int lb, ub = 0, 0;
-            
             int end_pt = min(size, static_cast<int>(weights.size()))
 
             for (int i = 0; i < range; i++) {
                 int weight = weights[i];
-
                 if (weight < 0) {
                     lb += weight;
                 }
@@ -31,8 +30,8 @@ class Bounds{
             return Bounds(lb, ub);
         } 
 
-    pair<int, int> get_bounds() const {
-        return make_pair(lower_bound, upper_bound);
+        pair<int, int> get_bounds() const {
+            return make_pair(lower_bound, upper_bound);
     }
 }
 
@@ -40,6 +39,7 @@ class ThresholdTest{
     private:
         int id_counter;
         vector<int> weights;
+        vector<int> indices;
         int threshold; 
         int size;
         Bounds bounds;
@@ -48,13 +48,15 @@ class ThresholdTest{
         static int new_id{
             return id_counter++;
         }
-      
-        ThresholdTest(const vector<int>& weights, int threshold)
-            : weights(weights), threshold(threshold) {
-            }
+        
+        ThresholdTest(const vector<int>& weights, int threshold,
+                    const vector<int>& indices, int size, const Bounds& bounds)
+                : weights(weights), threshold(threshold), indices(indices),
+                size(size), bounds(bounds), id(ThresholdTest::new_id()) {}
 
+    public:
         // gets last weight from list 
-        vector<int> get_last() const {
+        int get_last() const {
             return weights[size - 1];
         }
 
@@ -64,6 +66,9 @@ class ThresholdTest{
 
             if (value == 1) {
                 int nu_threshold = threshold - last_weight;
+            }
+            if (value == 0) {
+                int nu_threshold = threshold;
             }
 
             auto [lb, ub] = bounds.get_bounds();
@@ -78,6 +83,36 @@ class ThresholdTest{
             return make_shared<ThresholdTest>(weights, nu_threshold, indices, size - 1, nu_bounds);
 
         }
+
+        static pair<vector<int> vector<int>> sort_weights(const vector<int>& weights) {
+            vector<pair<int, int>> indexed_weights;
+            for (size_t i = 0; i < weights.size(); i++) {
+                indexed_weights.emplace_back(i, weights[i]);
+            }
+
+            sort(indexed_weights.begin(), indexed_weights.end(),
+                    [](const pair<int, int>& a, const pair<int, int>& b) {
+                        return abs(a.second) < abs(b.second);
+                    });
+
+            vector<int> indices;
+            vector<int> sorted_weights;
+            for (const auto& pair : indexed_weights) {
+                indices.push_back(pair.first);
+                sorted_weights.push_back(pair.second);
+            }
+
+            return make_pair(indices, sorted_weights);
+        }
+
+        bool trivial_pass() const {
+            auto [lower, upper] = bounds.get_bounds();
+            return threshold <= lower && lower <= upper;
+        }
+
+        bool trivial_fail() const {
+            auto [lower, upper] = bounds.get_bounds();
+            return lower <= upper && upper < threshold;
+        }
+
 }
-
-
